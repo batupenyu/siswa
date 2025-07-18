d90cb5f73a666d16ca00d65242b905b60580a6da
batupenyu <babelhendri@gmail.com>
2025-07-18 09:05:22 +0700

    commit

--- resources/views/akKredits/create.blade.php
+++ resources/views/akKredits/create.blade.php
@@ -17,7 +17,7 @@
                                 @foreach ($errors->all() as $error)
                                     <li>{{ $error }}</li>
                                 @endforeach
-                            </ul>
+                             </ul>
                         </div>
                     @endif
 
--- resources/views/pp_gaji/index.blade.php
+++ resources/views/pp_gaji/index.blade.php
@@ -22,14 +22,20 @@
             @foreach($ppGajis as $ppGaji)
             <tr>
                 <td>{{ $ppGaji->id }}</td>
-                <td>{{ $ppGaji->description }}</td>
-                <td>
-                    <a href="{{ route('pp_gaji.show', $ppGaji->id) }}" class="btn btn-info btn-sm">View</a>
-                    <a href="{{ route('pp_gaji.edit', $ppGaji->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                <td x>{{ $ppGaji->description }}</td>
+                <td style="width: 150px;"> 
+                    <a href="{{ route('pp_gaji.show', $ppGaji->id) }}" class="btn btn-info btn-sm" title="View">
+                        <i class="bi bi-eye"></i>
+                    </a>
+                    <a href="{{ route('pp_gaji.edit', $ppGaji->id) }}" class="btn btn-warning btn-sm" title="Edit">
+                        <i class="bi bi-pencil"></i>
+                    </a>
                     <form action="{{ route('pp_gaji.destroy', $ppGaji->id) }}" method="POST" style="display:inline-block;">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure to delete this item?')">Delete</button>
+                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure to delete this item?')" title="Delete">
+                            <i class="bi bi-trash"></i>
+                        </button>
                     </form>
                 </td>
             </tr>
