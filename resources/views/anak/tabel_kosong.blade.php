fc84f298d2267076b6b1240add849c4a14fb4db3
batupenyu <babelhendri@gmail.com>
2025-07-18 10:43:53 +0700

    commit

---
+++ resources/views/anak/tabel_kosong.blade.php
@@ -0,0 +1,27 @@
+<table class="family-table">
+    <tr>
+        <th rowspan="2" style="width: 20px;">NO</th>
+        <th rowspan="2">NAMA</th>
+        <th rowspan="2">TEMPAT LAHIR</th>
+        <th colspan="2">TANGGAL</th>
+        <th rowspan="2">PEKERJAAN</th>
+        <th rowspan="2">KETERANGAN</th>
+        <th rowspan="2">MENDAPATKAN TUNJANGAN</th>
+    </tr>
+    <tr>
+        <th>LAHIR</th>
+        <th>PERKAWINAN</th>
+    </tr>
+
+    <tr>
+        <td style="width: 20px;">-</td>
+        <td style="width: 20px;">-</td>
+        <td style="width: 20px;">-</td>
+        <td style="width: 20px;">-</td>
+        <td style="width: 20px;">-</td>
+        <td style="width: 20px;">-</td>
+        <td style="width: 20px;">-</td>
+        <td style="width: 20px;">-</td>
+    </tr>
+
+</table>
