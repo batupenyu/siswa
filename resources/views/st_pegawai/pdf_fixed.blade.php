cb28cd7559d7d9ff5704f9c79208f98a00b5298c
batupenyu <babelhendri@gmail.com>
2026-05-06 07:16:10 +0700

    commit

--- resources/views/st_pegawai/pdf_fixed.blade.php
+++ resources/views/st_pegawai/pdf_fixed.blade.php
@@ -131,7 +131,7 @@
         <td style="text-align: justify; vertical-align:auto;width:50px">:</td>
         <td style="vertical-align:top">
             <ol style="padding-left: 20px; padding-top: 0px; margin-top: 0px; text-align:justify">
-                <li>Dalam rangka {{ $stPegawai->nama_kegiatan }}</li>
+                <li>Dalam rangka {{ $stPegawai->nama_kegiatan }} di {{ $stPegawai->tempat_kegiatan }} tanggal {{ Carbon\Carbon::parse($stPegawai->tgl_awal)->translatedFormat('d F Y') }}.</li>
                 <li>Membuat laporan hasil pelaksanaan perintah tugas kepada {{$penilai->jabatan}} {{$penilai->unitkerja}}</li>
                 <li>Dilaksanakan dengan sebaik-baiknya dan penuh rasa tanggung jawab.</li>
             </ol>
